5283d15dee9e8dc5e728314d56875b4fbca3acb2
Sebastian Zapletal <s.zapletal@live.at>
2022-03-18 16:16:38 +0100

    arch experiment

--- inv_res.sh
+++ inv_res.sh
@@ -3,3 +3,7 @@
 python -m carla.train_agent --norm_obs --seed 44864 --exp_name inv_res --hid_context_net 0 --hid_state_net -1 --steps 2048
 python -m carla.train_agent --norm_obs --seed 26912 --exp_name inv_res --hid_context_net 0 --hid_state_net -1 --steps 2048
 
+
+python -m carla.train_agent --norm_obs --seed 44864 --exp_name inv_res_nbn --hid_context_net 0 --hid_state_net -2 --steps 2048
+python -m carla.train_agent --norm_obs --seed 26912 --exp_name inv_res_nbn --hid_context_net 0 --hid_state_net -2 --steps 2048
+
